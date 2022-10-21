3d7d4b89374444ebaf20ef0a26ec3a8f24ffa571
sodiqlef <kehindeogundimu731@gmail.com>
2022-10-21 22:37:49 +0100

    adding config

--- eurosport/settings.py
+++ eurosport/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
