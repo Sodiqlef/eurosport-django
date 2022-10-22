ed02d0dc0acdb3b00ccb4fb976980cc5f454c1db
sodiqlef <kehindeogundimu731@gmail.com>
2022-10-22 02:29:25 +0100

    Change logo font

--- eurosport/settings.py
+++ eurosport/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY') #''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['eurosports.herokuapp.com']
+ALLOWED_HOSTS = ['eurosports.herokuapp.com', 'localhost']
 
 
 # Application definition
